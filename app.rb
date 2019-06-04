7ad13fa7333f2c09db7e344d45c014b2ab8bbcc0
rudi hinds <rudihinds@gmail.com>
2019-06-04 13:17:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,11 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
+  
+  get "/multiply/:num1/:num2" do
+  	@product = params[:num1].to_i * params[:num2].to_i
+  	"#{@product}"
+  end
 
   # Code your final two routes here:
 
